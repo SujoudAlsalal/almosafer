52487ec0a6b6fabdde6799e63db233fc9f8c36d8
SujoudAlsalal <alsallalsojod0@gmail.com>
2024-07-07 15:59:12 +0300

    update2

---
+++ src/almosafer/Mysetup.java
@@ -0,0 +1,20 @@
+package almosafer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Mysetup {
+	WebDriver driver = new ChromeDriver();
+	String Url=("https://www.almosafer.com/en?ncr=1");
+	@BeforeTest
+	public void setup(){
+		driver.get(Url);
+	
+	}
+	
+	@Test
+	public void firsttest(){}
+
+}
